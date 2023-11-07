a204eea9c78ce9196e7f4cdab6d9504b30a3af80
gaston krasny <gukras@gmail.com>
2023-11-07 10:06:12 -0300

    add procfile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,6 @@ class ApplicationController < ActionController::Base
         rescue_from Exception, with: :render_error
         rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
         rescue_from ActionController::RoutingError, with: :render_not_found
-        rescue_from ActionController::UnknownController, with: :render_not_found
-        rescue_from ActionController::UnknownAction, with: :render_not_found
-        rescue_from ActionController::ParameterMissing, with: :render_not_found
     end      
     
     
